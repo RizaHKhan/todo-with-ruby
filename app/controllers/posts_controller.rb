21c1ee1a2e68299f552eb390cfa7f00a4ddc3b46
Riza Khan <khanriza@gmail.com>
2020-03-08 16:41:07 -0400

    Added Logic to Display Posts on Homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,23 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all
   end
 
-  def new 
-    
+  def show 
+    @post = Post.find(params[:id])
   end
 
-  def create 
-    render plain: params[:post].inspect
+  def new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
